bd7546ddfae83d081cbc24dc98f814e9d1a6165b
MaratKarsanov <marat.karsanov@mail.ru>
2024-01-30 16:43:51 +0300

    По запросу https://localhost:5001/calc/index?a=v1&b=v2&c=v3 выполняет операцию v3 над v1 и v2

---
+++ OnlineShopWebApp/Controllers/CalcController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        private readonly ILogger<CalcController> _logger;
+
+        public CalcController(ILogger<CalcController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index(double a, double b, string c)
+        {
+            if (c is null || c == "+")
+                return a + " + " + b + " = " + (a + b);
+            if (c == "-")
+                return a + " - " + b + " = " + (a - b);
+            if (c == "*")
+                return a + " * " + b + " = " + (a * b);
+            return "Неверная операция. Можно использовать только \"+\", \"-\", \"*\"!";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
