707d456f9e29f28df71c4c8bdced418be7fb7474
MaratKarsanov <marat.karsanov@mail.ru>
2024-01-30 16:31:07 +0300

    По запросу https://localhost:5001/calculator/index/a/b/operation выводит результат введенной операции над числами a и b

---
+++ OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CalculatorController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index(double a, double b, string operation)
+        {
+            if (operation is null || operation == "+")
+                return a + " + " + b + " = " + (a + b);
+            if (operation == "-")
+                return a + " - " + b + " = " + (a - b);
+            if (operation == "*")
+                return a + " * " + b + " = " + (a * b);
+            return "Неверная операция. Можно использовать только \"+\", \"-\", \"*\"!";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
